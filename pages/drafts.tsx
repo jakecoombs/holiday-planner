0942775836bafac12c3211fa33d5ff1a57c19946
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2023-02-05 17:21:19 +0000

    Add console.log

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -7,6 +7,7 @@ import prisma from "../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession({ req });
+
   if (!session) {
     res.statusCode = 403;
     return { props: { drafts: [] } };
@@ -34,6 +35,8 @@ type Props = {
 
 const Drafts: React.FC<Props> = (props) => {
   const { data: session } = useSession();
+
+  console.log("Session:", session);
 
   if (!session) {
     return (
